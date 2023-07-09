9f46fc620db95bfa589b759d2159c5d91256c892
Anirudh Gattu <104290279+agattu1@users.noreply.github.com>
2023-07-09 03:24:28 -0400

    ANIRUDH

--- ani.js
+++ ani.js
@@ -1,25 +1,13 @@
 // JavaScript
 const headingElement = document.getElementById('typing-effect');
-const subtitleElement = document.getElementById('typing-effect-subtitle');
-const headingText = 'Nano Artz Technologies';
-const subtitleText = 'Quality Over Quantity';
+const text = 'Nano Artz Technologies';
+let index = 0;
 
-let headingIndex = 0;
-let subtitleIndex = 0;
-
-function typeHeading() {
-  headingElement.textContent = headingText.slice(0, headingIndex++);
-  if (headingIndex <= headingText.length) {
-    setTimeout(typeHeading, 100); // Typing speed for heading (adjust as needed)
+function type() {
+  headingElement.textContent = text.slice(0, index++);
+  if (index <= text.length) {
+    setTimeout(type, 100); // Typing speed in milliseconds (adjust as needed)
   }
 }
 
-function typeSubtitle() {
-  subtitleElement.textContent = subtitleText.slice(0, subtitleIndex++);
-  if (subtitleIndex <= subtitleText.length) {
-    setTimeout(typeSubtitle, 100); // Typing speed for subtitle (adjust as needed)
-  }
-}
-
-typeHeading();
-setTimeout(typeSubtitle, 1000); // Delay before starting the subtitle typing effect (adjust as needed)
+type();
