7dd6542b00d4aea7ac2f587f1877c760684b3363
Anirudh Gattu <104290279+agattu1@users.noreply.github.com>
2023-07-09 05:30:07 -0400

    Orgin

---
+++ ani.js
@@ -0,0 +1,32 @@
+// JavaScript
+const headingElement = document.getElementById('typing-effect');
+const texts = ['YouTuber', 'Pythonist', 'Engineer'];
+let textIndex = 0;
+let charIndex = 0;
+
+function typeText() {
+  if (charIndex < texts[textIndex].length) {
+    headingElement.textContent += texts[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeText, 100); // Typing speed (adjust as needed)
+  } else {
+    setTimeout(eraseText, 1000); // Delay before erasing the text (adjust as needed)
+  }
+}
+
+function eraseText() {
+  const currentText = headingElement.textContent;
+  if (charIndex >= 0) {
+    headingElement.textContent = currentText.slice(0, charIndex);
+    charIndex--;
+    setTimeout(eraseText, 50); // Erasing speed (adjust as needed)
+  } else {
+    textIndex++;
+    if (textIndex >= texts.length) {
+      textIndex = 0; // Restart from the beginning once all texts are typed
+    }
+    setTimeout(typeText, 500); // Delay before typing the next text (adjust as needed)
+  }
+}
+
+typeText();
