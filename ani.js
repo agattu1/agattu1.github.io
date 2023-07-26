7a3110ebd9aa5d4710c2593ddec3a9372cd54c1f
Anirudh Gattu <104290279+agattu1@users.noreply.github.com>
2023-07-26 11:42:04 -0500

    Update ani.js

--- ani.js
+++ ani.js
@@ -1,6 +1,7 @@
 // JavaScript
 const headingElement = document.getElementById('typing-effect');
-const texts = ['athlete', 'engineer', 'believer'];
+//const texts = ['athlete', 'engineer', 'believer'];
+const texts = ['🔺 GARMIN Software Engineer', '⚽️ Fitness', '☁️ Cloud Enthusiast'];
 let textIndex = 0;
 let charIndex = 0;
 
